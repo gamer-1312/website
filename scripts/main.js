3b9469a4af92413bb0876976b63b2bef7aa83745
gamer-1312 <g00glelover@proton.me>
2024-08-05 01:21:10 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,9 @@ const changeIMG = document.querySelector("img");
 
 changeIMG.onclick = () => {
     const mySrc = changeIMG.getAttribute("src");
-    if (mySrc === "file:///home/laura/Desktop/website/images/trollface.png") {
-        changeIMG.setAttribute("src", "file:///home/laura/Desktop/website/images/not_trollface.png");
+    if (mySrc === "./images/trollface.png") {
+        changeIMG.setAttribute("src", "./images/not_trollface.png");
     } else {
-        changeIMG.setAttribute("src", "file:///home/laura/Desktop/website/images/trollface.png");
+        changeIMG.setAttribute("src", "./images/trollface.png");
     }
 };
